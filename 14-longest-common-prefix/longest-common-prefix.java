f693968b1c0a1b23a79fae8300fed3df5740bcbb
Viswavel <viswavel007_mca27@mepcoeng.ac.in>
2026-08-05 10:00:59 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (92.63%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,25 +1,20 @@
-import java.util.Arrays;
-
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) {
             return "";
         }
         
-        Arrays.sort(strs);
+        String prefix = strs[0];
         
-        String first = strs[0];
-        String last = strs[strs.length - 1];
-        int i = 0;
-        
-        while (i < first.length() && i < last.length()) {
-            if (first.charAt(i) == last.charAt(i)) {
-                i++;
-            } else {
-                break;
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
             }
         }
         
-        return first.substring(0, i);
+        return prefix;
     }
 }
